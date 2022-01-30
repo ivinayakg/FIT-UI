6757c6bdcfadcdd01c92bb3b3ba9c222d8a8c207
Vinayak <vinayak20029@gmail.com>
2022-01-31 00:11:39 +0530

    fixed the avatar components

--- COMPONENTS/avatar/index.js
+++ COMPONENTS/avatar/index.js
@@ -1,4 +1,4 @@
-const avatars = document.querySelectorAll(".avatar");
+const avatars = document.querySelectorAll(".avatar_div");
 
 avatars.forEach((target, i) => {
   target.style.backgroundColor = target.attributes.color.nodeValue;
