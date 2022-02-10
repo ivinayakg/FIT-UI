8bc922503ce910103a38d85dd9f1c06a090df431
Vinayak <vinayak20029@gmail.com>
2022-02-10 14:29:41 +0530

    librabry completed and merged into CSS, JS folder

---
+++ JS/main.mjs
@@ -0,0 +1,25 @@
+import {
+  alertHandler,
+  avatarHandler,
+  badgeHandler,
+  gridHandler,
+  inputHandler,
+  listHandler,
+  modalHandler,
+  navigationHandler,
+  ratingHandler,
+  sliderHandler,
+  snackbarHandler,
+} from "./component";
+
+alertHandler();
+avatarHandler();
+badgeHandler();
+gridHandler();
+inputHandler();
+listHandler();
+modalHandler();
+navigationHandler();
+ratingHandler();
+sliderHandler();
+snackbarHandler();
