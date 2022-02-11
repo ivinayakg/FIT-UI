9fce1c554d75a257670ad22b0e0ad005928bf249
Vinayak <vinayak20029@gmail.com>
2022-02-12 03:43:28 +0530

    frontend documentation completed

--- docs/main.js
+++ docs/main.js
@@ -15,24 +15,31 @@ const docsLinks = [
   "rating",
   "slider",
   "snackbar",
-  "text",
 ];
 const Frame = document.querySelector(".docs_frame");
 const themeToggler = document.querySelector(".header_themeToggle");
 
 docsLinksEl.forEach((target, i) => {
   target.addEventListener("click", () => {
+    Frame.onload = () => {
+      let body = document.querySelector("body");
+      let frameBody = Frame.contentWindow.document.querySelector("body");
+      if (body.className.includes("dark-theme"))
+        frameBody.classList.add("dark-theme");
+
+      Frame.style.display = "block";
+    };
+
     Frame.attributes["src"].nodeValue = `/docs/pages/${docsLinks[i]}.html`;
+    Frame.style.display = "none";
   });
 });
 
 themeToggler.addEventListener("click", () => {
   let frameBody = Frame.contentWindow.document.querySelector("body");
-  if (frameBody.className.includes("dark-theme")) {
+  if (frameBody.className.includes("dark-theme"))
     frameBody.classList.remove("dark-theme");
-  } else {
-    frameBody.classList.add("dark-theme");
-  }
+  else frameBody.classList.add("dark-theme");
 });
 
 window.scroll(0, -10);
--- docs/pages/docs.js
+++ docs/pages/docs.js
@@ -1,6 +1,7 @@
 const docsTitle = document.querySelector(".docs_titles");
-const headings = document.querySelectorAll("h2");
+const headings = document.querySelectorAll(".head2");
 const docContainer = document.querySelector(".docs_container");
+const body = document.querySelector("body");
 
 const linkGenerator = (value, y) => {
   let link = document.createElement("span");
@@ -12,14 +13,20 @@ const linkGenerator = (value, y) => {
   return link;
 };
 
-headings.forEach((target, i) => {
-  let link = linkGenerator(target.innerText, target.getBoundingClientRect().y);
-  if (i === 0) {
-    link = linkGenerator(target.innerText, 0);
-  }
-  if (docsTitle) {
-    docsTitle.append(link);
-  }
-});
+setTimeout(() => {
+  headings.forEach((target, i) => {
+    let link = linkGenerator(
+      target.innerText,
+      target.getBoundingClientRect().y
+    );
+
+    if (i === 0) {
+      link = linkGenerator(target.innerText, 0);
+    }
+    if (docsTitle) {
+      docsTitle.append(link);
+    }
+  });
+}, 1500);
 
 window.scroll(0, -10);
